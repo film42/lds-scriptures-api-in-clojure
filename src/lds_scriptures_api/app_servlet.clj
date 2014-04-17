f9923c898cdcab366915369243e6db83142cf359
Garrett Thornburg <film42@gmail.com>
2014-04-17 00:00:46 -0600

    Remove beta app engine deps

--- src/lds_scriptures_api/app_servlet.clj
+++
@@ -1,8 +0,0 @@
-(ns lds-scriptures-api.app_servlet
-  (:gen-class :extends javax.servlet.http.HttpServlet)
-  (:use lds-scriptures-api.core)
-  (:use [appengine-magic.servlet :only [make-servlet-service-method]]))
-
-
-(defn -service [this request response]
-  ((make-servlet-service-method lds-scriptures-api-app) this request response))
--- src/lds_scriptures_api/core.clj
+++
@@ -1,5 +0,0 @@
-(ns lds-scriptures-api.core
-  (:use lds-scriptures-api.handler)
-  (:require [appengine-magic.core :as ae]))
-
-(ae/def-appengine-app lds-scriptures-api-app #'app)
