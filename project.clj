552b9b1f5c7b359739ba168d93a1059e7838ee74
Garrett Thornburg <film42@gmail.com>
2014-06-23 17:58:22 -0600

    set project to AOT all the things

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject lds-scriptures-api "0.1.0-SNAPSHOT"
   :min-lein-version "2.0.0"
+  :aot :all
   :description "Fancy scripture api in clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]
 
